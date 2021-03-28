7aff7beaed6db580c022ba8dcbe79e7acef0a41e
JesusRRr <rolonjesus1996@gmail.com>
2021-03-27 20:09:57 -0600

    chore: paddle_a, paddle_c and ball created

---
+++ pong_game/pong.py
@@ -0,0 +1,39 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Pong")
+window.bgcolor("black")
+window.setup(width=800, height=600)
+window.tracer(0)
+
+# Paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) #set speed at max
+paddle_a.shape("square")
+paddle_a.color("white")
+paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.penup()
+paddle_a.goto(-350, 0)
+
+
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0) #set speed at max
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
+
+#Ball
+ball = turtle.Turtle()
+ball.speed(0) #set speed at max
+ball.shape("square")
+ball.color("white")
+ball.shapesize(stretch_wid=1, stretch_len=1)
+ball.penup()
+ball.goto(0, 0)
+
+
+while True:
+    window.update()
